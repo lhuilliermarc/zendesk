9f387d4511ee18eb0291498dd587ac3e1499d8fc
lhuilliermarc <lhuilliermarc@free.fr>
2026-03-15 18:25:02 +0100

    création de projet

--- app/action.ts
+++ app/action.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import prisma from "@/lib/prisma";
+import { randomBytes } from "crypto";
 
 export async function checkAndAddUser(email: string, name: string) {
     if (!email) return
@@ -23,5 +24,37 @@ export async function checkAndAddUser(email: string, name: string) {
         }
     } catch (error) {
         console.error("Erreur lors de la vérification de l'utilisateur:", error);
+    }
+}
+
+function generateUniqueCode(): string {
+    return randomBytes(6).toString('hex')
+}
+
+export async function createProject(name: string, description: string, email: string) {
+    try {
+
+        const inviteCode = generateUniqueCode()
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const newProject = await prisma.project.create({
+            data: {
+                name,
+                description,
+                inviteCode,
+                createdById: user.id
+            }
+        })
+        return newProject
+    } catch (error) {
+        console.error(error)
+        throw new Error
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,74 @@
+"use client"
+
 import Image from "next/image";
 import Wrapper from "./components/Wrapper";
+import { useState } from "react";
+import { FolderGit2 } from "lucide-react";
+import { createProject } from "./action";
+import { useUser } from "@clerk/nextjs";
+import { toast } from "react-toastify";
 
 export default function Home() {
+
+  const { user } = useUser()
+  const email = user?.primaryEmailAddress?.emailAddress as string
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+
+  const handleSubmit = async () => {
+    try {
+      const modal = document.getElementById('my_modal_3') as HTMLDialogElement
+      const project = await createProject(name, description, email)
+      if (modal) {
+        modal.close()
+      }
+      setName(""),
+        setDescription("")
+      toast.success("Projet créé")
+    } catch (error) {
+      console.error('Error creating project', error);
+    }
+  }
+
   return (
     <Wrapper>
-      <button className="btn btn-accent">test</button>
+      <div>
+        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+        <button className="btn btn-primary mb-6" onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>Nouveau Projet <FolderGit2 /></button>
+
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <h3 className="font-bold text-lg">Nouveau Projet</h3>
+            <p className="py-4">Décrivez votre projet simplement grâce à la description</p>
+            <div>
+              <input
+                placeholder="Nom du projet"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="border border-base-300 input input-bordered w-full mb-4 placeholder:text-sm"
+                required
+              />
+              <textarea
+                placeholder="Description"
+                value={description}
+                rows={4}
+                onChange={(e) => setDescription(e.target.value)}
+                className="mb-2 textarea text-area-bprdered border border-base-300 w-full textarea-md placeholder:text-sm"
+                required
+              >
+              </textarea>
+              <button className="btn btn-primary" onClick={handleSubmit}>
+                Nouveau Projet <FolderGit2 />
+              </button>
+            </div>
+          </div>
+        </dialog>
+      </div>
     </Wrapper>
   );
 }
